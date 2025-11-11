e373cc711b16c67e0216049ba77f2b7090116e52
Agustincito <agus@poap.io>
2025-11-11 10:10:19 -0300

    Aave: Supply with contract call

--- examples/5-invest-aave-idle-balance/src/task.ts
+++ examples/5-invest-aave-idle-balance/src/task.ts
@@ -1,32 +1,38 @@
-import { BigInt, environment, ERC20Token, log, Swap, TokenAmount, USD } from '@mimicprotocol/lib-ts'
+import { BigInt, DenominationToken, ERC20Token, log, TokenAmount, USD } from '@mimicprotocol/lib-ts'
 
+import { AavePool } from './types/AavePool'
 import { AaveToken } from './types/AaveToken'
 import { ERC20 } from './types/ERC20'
 import { inputs } from './types'
 
 export default function main(): void {
-  if (inputs.slippage > 100) throw new Error('Slippage must be between 0 and 100')
-
   const aToken = ERC20Token.fromAddress(inputs.aToken, inputs.chainId)
   const aTokenContract = new AaveToken(aToken.address, aToken.chainId)
 
   const underlyingTokenAddress = aTokenContract.UNDERLYING_ASSET_ADDRESS()
   const underlyingToken = ERC20Token.fromAddress(underlyingTokenAddress, aToken.chainId)
 
-  const me = environment.getContext().user
+  const aavePool = new AavePool(aTokenContract.POOL(), inputs.chainId)
+
   const underlyingTokenContract = new ERC20(underlyingToken.address, underlyingToken.chainId)
-  const underlyingTokenBalanceAmount = underlyingTokenContract.balanceOf(me)
+  const underlyingTokenBalanceAmount = underlyingTokenContract.balanceOf(inputs.smartAccount)
   const underlyingTokenBalance = TokenAmount.fromBigInt(underlyingToken, underlyingTokenBalanceAmount)
   const underlyingTokenBalanceInUsd = underlyingTokenBalance.toUsd()
   const thresholdUsd = USD.fromI32(inputs.thresholdUSD)
   log.info('Underlying balance in USD: ' + underlyingTokenBalanceInUsd.toString())
 
-  if (underlyingTokenBalanceInUsd.lt(thresholdUsd)) log.info('Threshold not met')
-  else {
-    const slippagePct = BigInt.fromI32(100).minus(BigInt.fromI32(inputs.slippage))
-    const minAmountOut = underlyingTokenBalance.toTokenAmount(aToken).times(slippagePct).div(BigInt.fromI32(100))
-    log.info('Min amount out: ' + minAmountOut.toString())
-
-    Swap.create(inputs.chainId, underlyingToken, underlyingTokenBalanceAmount, aToken, minAmountOut.amount).send()
+  if (underlyingTokenBalanceInUsd.lt(thresholdUsd)) {
+    log.info('Threshold not met')
+    return
   }
+
+  const calls = underlyingTokenContract.approve(aavePool.address, underlyingTokenBalanceAmount)
+
+  calls.addCallsFromBuilder(
+    aavePool.supply(underlyingToken.address, underlyingTokenBalance.amount, inputs.smartAccount, 0)
+  )
+
+  // Use mimic credits to pay for the transaction fee
+  const feeWithCredits = TokenAmount.fromStringDecimal(DenominationToken.USD(), inputs.maxFee)
+  calls.addUser(inputs.smartAccount).addMaxFee(feeWithCredits).build().send()
 }
