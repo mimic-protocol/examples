af53fa039048e78d35e48586931fa1713e538e6c
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2025-11-13 20:03:58 -0300

    examples: add relevant tokens query example

---
+++ examples/08-relevant-tokens-query/src/task.ts
@@ -0,0 +1,30 @@
+import {
+  BigInt,
+  DenominationToken,
+  environment,
+  ListType,
+  log,
+  TokenAmount,
+  TransferBuilder,
+  USD,
+} from '@mimicprotocol/lib-ts'
+
+import { inputs } from './types'
+
+export default function main(): void {
+  const context = environment.getContext()
+  const tokens = environment.getRelevantTokens(context.user, [inputs.chainId], USD.zero(), [], ListType.AllowList)
+  const builder = TransferBuilder.forChain(inputs.chainId)
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i]
+    if (token.amount.gt(BigInt.zero())) {
+      builder.addTransferFromTokenAmount(token, inputs.recipient)
+      log.info(`Adding transfer for ${token} on chain ${inputs.chainId}`)
+    }
+  }
+
+  if (builder.transfers.length > 0) {
+    builder.addMaxFee(TokenAmount.fromStringDecimal(DenominationToken.USD(), inputs.feeAmountUsd)).build().send()
+  }
+}
---
+++ examples/08-relevant-tokens-query/tests/task.spec.ts
@@ -0,0 +1,119 @@
+import { NATIVE_TOKEN_ADDRESS, OpType, randomEvmAddress } from '@mimicprotocol/sdk'
+import { ContractCallMock, GetRelevantTokensMock, runTask, Transfer } from '@mimicprotocol/test-ts'
+import { expect } from 'chai'
+
+describe('Task', () => {
+  const taskDir = './build'
+
+  const chainId = 1
+  const USDC = randomEvmAddress()
+  const USDT = randomEvmAddress()
+
+  const context = {
+    user: randomEvmAddress(),
+    settlers: [{ address: randomEvmAddress(), chainId }],
+    timestamp: 1438223173000,
+  }
+
+  const inputs = {
+    chainId,
+    feeAmountUsd: '0.1',
+    recipient: randomEvmAddress(),
+  }
+
+  const calls: ContractCallMock[] = [
+    {
+      request: { chainId, to: USDC, fnSelector: '0x313ce567' },
+      response: { value: '6', abiType: 'uint8' },
+    },
+    {
+      request: { chainId, to: USDT, fnSelector: '0x313ce567' },
+      response: { value: '6', abiType: 'uint8' },
+    },
+    {
+      request: { chainId, to: USDC, fnSelector: '0x95d89b41' },
+      response: { value: 'USDC', abiType: 'string' },
+    },
+    {
+      request: { chainId, to: USDT, fnSelector: '0x95d89b41' },
+      response: { value: 'USDT', abiType: 'string' },
+    },
+  ]
+
+  describe('when the user has some balance for the requested tokens', () => {
+    const relevantTokens: GetRelevantTokensMock[] = [
+      {
+        request: {
+          owner: context.user,
+          chainIds: [chainId],
+          usdMinAmount: '0',
+          tokenFilter: 0,
+          tokens: [],
+        },
+        response: [
+          {
+            timestamp: Date.now(),
+            balances: [
+              { token: { address: USDC, chainId }, balance: '10' },
+              { token: { address: NATIVE_TOKEN_ADDRESS, chainId }, balance: '100' },
+            ],
+          },
+        ],
+      },
+    ]
+
+    it('produces the expected intents for multiple tokens', async () => {
+      const result = await runTask(taskDir, context, { inputs, calls, relevantTokens })
+      expect(result.success).to.be.true
+      expect(result.timestamp).to.be.equal(context.timestamp)
+
+      const intents = result.intents as Transfer[]
+      expect(intents).to.have.lengthOf(1)
+
+      expect(intents[0].op).to.be.equal(OpType.Transfer)
+      expect(intents[0].chainId).to.equal(chainId)
+      expect(intents[0].maxFees.length).to.equal(1)
+      expect(intents[0].maxFees[0].token).to.equal('0x0000000000000000000000000000000000000348')
+      expect(intents[0].maxFees[0].amount).to.equal('100000000000000000')
+      expect(intents[0].user).to.equal(context.user)
+      expect(intents[0].transfers).to.have.lengthOf(2)
+
+      const firstTransfer = intents[0].transfers[0]
+      expect(firstTransfer.token).to.equal(USDC)
+      expect(firstTransfer.amount).to.equal('10')
+      expect(firstTransfer.recipient).to.be.equal(inputs.recipient)
+
+      const secondTransfer = intents[0].transfers[1]
+      expect(secondTransfer.token).to.equal(NATIVE_TOKEN_ADDRESS.toLowerCase())
+      expect(secondTransfer.amount).to.equal('100')
+      expect(secondTransfer.recipient).to.be.equal(inputs.recipient)
+    })
+  })
+
+  describe('when the user does not have balance for the requested tokens', () => {
+    const relevantTokens: GetRelevantTokensMock[] = [
+      {
+        request: {
+          owner: context.user,
+          chainIds: [chainId],
+          usdMinAmount: '0',
+          tokenFilter: 0,
+          tokens: [],
+        },
+        response: [
+          {
+            timestamp: Date.now(),
+            balances: [{ token: { address: USDC, chainId }, balance: '0' }],
+          },
+        ],
+      },
+    ]
+
+    it('does not produce any intents', async () => {
+      const result = await runTask(taskDir, context, { inputs, calls, relevantTokens })
+      expect(result.success).to.be.true
+      expect(result.timestamp).to.be.equal(context.timestamp)
+      expect(result.intents).to.be.empty
+    })
+  })
+})
