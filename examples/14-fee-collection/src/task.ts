95b7a904642da0d237085b46c6ec72374450a439
lgalende <lau@mimic.fi>
2025-11-25 16:36:16 -0300

    examples: add fee collection example

---
+++ examples/14-fee-collection/src/task.ts
@@ -0,0 +1,71 @@
+import {
+  Arbitrum,
+  Base,
+  BigInt,
+  BlockchainToken,
+  ChainId,
+  environment,
+  ERC20Token,
+  ListType,
+  log,
+  Optimism,
+  SwapBuilder,
+  Token,
+  USD,
+} from '@mimicprotocol/lib-ts'
+
+import { inputs } from './types'
+
+const BPS_DENOMINATOR = BigInt.fromI32(10_000)
+
+export default function main(): void {
+  const chainId = inputs.chainId
+  const context = environment.getContext()
+
+  // Find tokens with user's balance > 0
+  const tokenList = buildTokenList(inputs.tokensCsv, chainId)
+  const amountsIn = environment.getRelevantTokens(context.user, [chainId], USD.zero(), tokenList, ListType.AllowList)
+
+  if (amountsIn.length == 0) {
+    log.info(`No tokens found on chain ${chainId}`)
+    return
+  }
+
+  const USDC = getUsdc(chainId)
+  const slippageFactor = BPS_DENOMINATOR.minus(BigInt.fromI32(inputs.slippageBps as i32))
+
+  for (let i = 0; i < amountsIn.length; i++) {
+    const amountIn = amountsIn[i]
+    const amountOut = amountIn.toTokenAmount(USDC)
+    const minAmountOut = amountOut.times(slippageFactor).div(BPS_DENOMINATOR)
+
+    // Note that the recipient will receive the USDC
+    SwapBuilder.forChain(chainId)
+      .addTokenInFromTokenAmount(amountIn)
+      .addTokenOutFromTokenAmount(minAmountOut, inputs.recipient)
+      .build()
+      .send()
+
+    log.info(`Adding swap of ${amountIn} to ${minAmountOut} on chain ${chainId}`)
+  }
+}
+
+function buildTokenList(tokensCsv: string, chainId: u32): BlockchainToken[] {
+  const list = new Array<BlockchainToken>()
+
+  const tokenAddresses = tokensCsv.split(',')
+  for (let i = 0; i < tokenAddresses.length; i++) {
+    const tokenAddress = tokenAddresses[i]
+    const erc20 = ERC20Token.fromString(tokenAddress, chainId)
+    list.push(changetype<BlockchainToken>(erc20))
+  }
+
+  return list
+}
+
+function getUsdc(chainId: i32): Token {
+  if (chainId == ChainId.ARBITRUM) return Arbitrum.USDC
+  if (chainId == ChainId.BASE) return Base.USDC
+  if (chainId == ChainId.OPTIMISM) return Optimism.USDC
+  throw new Error('Invalid chain')
+}
