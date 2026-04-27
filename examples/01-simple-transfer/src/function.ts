e669a1c02390b818603228ee2dcfb7e510453aec
PedroAraoz <paraoz22@gmail.com>
2026-04-27 10:54:19 -0300

    improve 01 readability

--- examples/01-simple-transfer/src/function.ts
+++ examples/01-simple-transfer/src/function.ts
@@ -1,8 +1,10 @@
 import { Address, ChainId, Optimism, TokenAmount, TransferBuilder } from '@mimicprotocol/lib-ts'
 
 export default function main(): void {
-  const chainId = ChainId.OPTIMISM
   const recipient = Address.fromString('0xbcE3248eDE29116e4bD18416dcC2DFca668Eeb84')
   const maxFee = TokenAmount.fromStringDecimal(Optimism.USDC, '1')
-  TransferBuilder.forChain(chainId).addTransferFromStringDecimal(Optimism.USDC, '1', recipient).build().send(maxFee)
+  const transfer = TransferBuilder.forChain(ChainId.OPTIMISM)
+    .addTransferFromStringDecimal(Optimism.USDC, '1', recipient)
+    .build()
+  transfer.send(maxFee)
 }
